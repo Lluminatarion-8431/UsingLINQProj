75ae7b5acbbae3874773afaa885b61633cff4c46
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-09-08 10:29:16 -0700

    created chooseDistinctName Method which returns a copy of names wihout duplicates and classGradeAverage method which calculates and returns with the class grade average of the average.

--- UsingLINQ.cs
+++ UsingLINQ.cs
@@ -19,5 +19,34 @@ namespace LINQ
                 Console.WriteLine(word);
             }
         }
+
+        public void chooseDistinctName()
+        {
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            var distinctNames = names.Distinct();
+            foreach(string name in distinctNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public void classGradeAverage()
+        {
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+
+            var averageString = classGrades
+                .Select(x => x.Split(','))
+                .Select(x => Array.ConvertAll(x, double.Parse))
+                .Select(x => (x.Sum() - x.Min()) / (x.Count() - 1))
+                .Average();
+            Console.WriteLine(averageString);
+            Console.ReadLine();
+        }
     }
 }
