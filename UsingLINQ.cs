51195201b43d44d7a64611da41b900a95c787392
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-09-08 09:57:28 -0700

    Init. Created returnWordsWithTH() method.

---
+++ UsingLINQ.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class UsingLINQ
+    {
+        public void returnWordsWithTH()
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "tenth", "mathematics" };
+            var Words = from word in words
+                        where word.Contains("th")
+                        select word;
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
